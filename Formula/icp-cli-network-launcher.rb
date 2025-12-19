883c7ae85e5266306a4870b1ea66b869d6c21ca9
Linwei Shang <linwei.shang@dfinity.org>
2025-12-19 11:12:43 -0500

    Add icp-cli-network-launcher formula

---
+++ Formula/icp-cli-network-launcher.rb
@@ -0,0 +1,16 @@
+class IcpCliNetworkLauncher < Formula
+  desc "CLI interface to pocket-ic, used with icp-cli"
+  homepage "https://github.com/dfinity/icp-cli-network-launcher"
+  url "https://github.com/dfinity/icp-cli-network-launcher/archive/refs/tags/v11.0.0.tar.gz"
+  sha256 "d4cf98ef6cd721533318124bc600a344e17391f764d3663f755b2539bb6035ba"
+  license "Apache-2.0"
+
+  depends_on "jq" => :build
+  depends_on "rust" => :build
+
+  def install
+    system "./package.sh", "out"
+    bin.install "out/icp-cli-network-launcher"
+    bin.install "out/pocket-ic"
+  end
+end
