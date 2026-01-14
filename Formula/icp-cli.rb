f441ea842750722861e0d12437fa0148026c43d5
Linwei Shang <linwei.shang@dfinity.org>
2026-01-13 19:42:33 -0500

    chore: release icp-cli v0.1.0-beta.3 (#14)
    Co-authored-by: Adam Spofford <adam.spofford@dfinity.org>
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- Formula/icp-cli.rb
+++ Formula/icp-cli.rb
@@ -1,33 +1,28 @@
 class IcpCli < Formula
   desc "Development tool for building and deploying canisters on ICP"
   homepage "https://github.com/dfinity/icp-cli"
-  url "https://github.com/dfinity/icp-cli/archive/refs/tags/v0.1.0-beta.2.tar.gz"
-  sha256 "e3ee62316142805aa5254c61cfb6bbb8aa6579c746538db7792d777edd62b576"
+  url "https://github.com/dfinity/icp-cli/archive/refs/tags/v0.1.0-beta.3.tar.gz"
+  sha256 "6a7740f4d4b3171ad53ef614f0311e502dad527d8427e63d78ee90afdf2a8c74"
   license "Apache-2.0"
 
   bottle do
-    root_url "https://github.com/dfinity/homebrew-tap/releases/download/icp-cli-0.1.0-beta.2"
-    sha256 cellar: :any,                 arm64_sequoia: "3e5157dce96e5b8b75a373aac87ca035315f0af97bc34dc43167d41904439766"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "aafd9a9b4562b5750e31a2c06d92bf658076dbbcff17d2723b2ae4f922c7fb8a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e4ff65138f74564b0eac60466a0372b907159a85a95ff497d8e323eb1c74546a"
+    root_url "https://github.com/dfinity/homebrew-tap/releases/download/icp-cli-0.1.0-beta.3"
+    sha256 cellar: :any,                 arm64_sequoia: "9b965964152aef33da94d8a9a07187bedb2a13885389c625ab747dd58cf6e255"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "3ed7db6facc02d7ad2a88ad2804bde985e250850028a1df0f42867bf5d143778"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3a3ccadddcb9adf26e14e4485954d3020a570f2fcfe90806492fde9c1bff69ab"
   end
 
   depends_on "rust" => :build
   depends_on "ic-wasm"
-  depends_on "icp-cli-network-launcher"
   depends_on "openssl@3"
   depends_on "zlib"
 
-  def install
-    with_env(ICP_CLI_VERSION: version.to_s) do
-      system "cargo", "install", *std_cargo_args(root: libexec, path: "crates/icp-cli")
-    end
+  on_linux do
+    depends_on "dbus"
+  end
 
-    keg_launcher = Formula["icp-cli-network-launcher"].opt_bin/"icp-cli-network-launcher"
-    icp_env = {
-      ICP_CLI_NETWORK_LAUNCHER_PATH: "${ICP_CLI_NETWORK_LAUNCHER_PATH:-#{keg_launcher}}",
-    }
-    (bin/"icp").write_env_script libexec/"bin/icp", icp_env
+  def install
+    system "cargo", "install", *std_cargo_args(path: "crates/icp-cli")
   end
 
   test do
